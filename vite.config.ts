91045e0fa915953b0c0c6d807af850654686b063
LongKN <enzoochen@outlook.com>
2025-06-13 09:19:18 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/enzokn.github.io/',
   plugins: [
     tailwindcss(),
     react(),
   ],
+  base: '/enzokn.github.io/',
 })
